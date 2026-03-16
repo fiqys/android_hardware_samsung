d137adeb23e9744d2c2c13609da2e48d852c264f
fiqys <elsifycritic@gmail.com>
2026-03-17 00:11:10 +0300

    Delete doze/src/org/lineageos/settings/doze directory

--- doze/src/org/lineageos/settings/doze/ScreenType.java
+++
@@ -1,9 +0,0 @@
-package org.lineageos.internal.util;
-
-import android.content.Context;
-
-public class ScreenType {
-    public static boolean isTablet(Context context) {
-        return context.getResources().getConfiguration().smallestScreenWidthDp >= 600;
-    }
-}
